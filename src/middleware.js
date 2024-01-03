9276f286ec9cc3af255e0a05af5dde76f1aab078
tasniaarhamarham <tasniaarhamarham@gmail.com>
2024-01-03 20:03:16 +0600

    done

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,23 @@
 import { NextResponse } from "next/server";
 import { verifyToken } from "./Utility/JWTcreate";
 
-export const middleware=async(req,res)=>{
-    let cookievalue=req.cookies.get('token');
-    let tokenvalue=await verifyToken(cookievalue['value'])
-    console.log(tokenvalue.payload)
-    const headerslist=new Headers(req.headers)
-    headerslist.set('email',tokenvalue.payload['email'])
-    headerslist.set('id',tokenvalue.payload['id'])
+export const middleware = async (req, res) => {
+  let cookievalue = req.cookies.get('token');
 
-    return NextResponse.next({status:"200",message:"successfull",request:{headers:headerslist}})
-}
+  if (cookievalue) {
+    let tokenvalue = await verifyToken(cookievalue['value']);
+    
+    if (tokenvalue) {
+      console.log(tokenvalue.payload);
+      const headerslist = new Headers(req.headers);
+      headerslist.set('email', tokenvalue?.payload['email']);
+      headerslist.set('id', tokenvalue?.payload['id']);
+
+      return NextResponse.next({ status: "200", message: "successful", request: { headers: headerslist } });
+    } else {
+      return NextResponse.next({ status: "401", message: "Invalid token" });
+    }
+  } else {
+    return NextResponse.next({ status: "401", message: "Token not found in cookies" });
+  }
+};
